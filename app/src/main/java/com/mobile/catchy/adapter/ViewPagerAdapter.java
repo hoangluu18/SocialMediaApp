34959c68ad1fefabba5103fac1280afa4485a06a
Lưu Việt Hoàng <luu186835@gmail.com>
2024-10-15 17:22:07 +0700

    Initial commit for PagerAdapter and fragments: Add, Home, Search, Notification, Profile to support tabbed layout.

---
+++ app/src/main/java/com/mobile/catchy/adapter/ViewPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.mobile.catchy.adapter;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.mobile.catchy.fragments.Add;
+import com.mobile.catchy.fragments.Home;
+import com.mobile.catchy.fragments.Notification;
+import com.mobile.catchy.fragments.Profile;
+import com.mobile.catchy.fragments.Search;
+
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int noOfTabs;
+
+    public PagerAdapter(@NonNull FragmentManager fm, int noOfTabs) {
+        super(fm);
+        this.noOfTabs = noOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new Home();
+
+            case 1:
+                return new Search();
+
+            case 2:
+                return new Add();
+
+            case 3:
+                return new Notification();
+
+            case 4:
+                return new Profile();
+            default:
+                return null;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return noOfTabs;
+    }
+}
